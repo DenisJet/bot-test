dab847f22b8192a4baa2692a208f2e7a3ff12662
Denis Kalkopf <ddenproton@protonmail.com>
2025-02-08 10:51:32 +0500

    refactor: bot color and bot name

--- components/BotsList/BotsList.tsx
+++ components/BotsList/BotsList.tsx
@@ -1,4 +1,3 @@
-import { getBotColor } from "@/helpers/getBotColor";
 import { Bot } from "@/interfaces/data.Interface";
 import BotIcon from "@/public/bot.svg";
 
@@ -8,15 +7,17 @@ export default function BotsList({ bots }: { bots: Bot[] }) {
       <ul className="grid grid-cols-3 gap-1">
         {bots &&
           bots.map((bot) => {
+            const botName = bot.name.split("_").join("").toUpperCase();
+            const botColor = bot.name.split("_")[0];
             return (
               <li key={bot.name}>
                 <div className="border rounded-sm bg-gray-800 p-3 w-full h-full flex flex-col justify-center items-center">
                   <BotIcon
-                    className={getBotColor(bot.name)}
+                    style={{ color: `${botColor}` }}
                     width={48}
                     height={48}
                   />
-                  <div>{bot.name}</div>
+                  <div className="text-xs">{botName}</div>
                 </div>
               </li>
             );
